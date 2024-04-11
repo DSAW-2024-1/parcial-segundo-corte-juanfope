6c53e871848b05f36457739b8bb9b79b96ead374
juanfope <157375092+juanfope@users.noreply.github.com>
2024-04-11 11:54:37 -0500

    ./-1st-endpoint-fix

--- API.js
+++ API.js
@@ -3,6 +3,12 @@ const axios = require("axios");
 const app = express();
 const PORT = 8080;
   
+app.get('/', (req, res) => {
+  console.log("accedio sin problemas a /")
+  res.status(200).send("Ha accedido a localhost:8080")
+  //res.send("Ha accedido a localhost:8080")
+});
+
 // Endpoint para obtener la lista de activos
 app.get('/coin/:coinName', async (req, res) => {
   try {
@@ -10,15 +16,11 @@ app.get('/coin/:coinName', async (req, res) => {
     const response = await axios.get(`https://api.coincap.io/v2/assets/${coinName}`);
     const coinData = response.data.data;
 
-    if (!coinData) {
-      return res.status(404).send('El nombre de la moneda no fue encontrado en la base de datos');
-    }
-
     const priceUSD = coinData.priceUsd;
     return res.send(`El precio en dólares de ${coinName} para el día de hoy es ${priceUSD}`);
   } catch (error) {
-    console.error('Error al consultar la API de CoinCap:', error.message);
-    return res.status(500).send('Error al consultar la API de CoinCap');
+    console.error('El nombre de la moneda no fue encontrado en la base de datos', error.message);
+    return res.status(404).send('El nombre de la moneda no fue encontrado en la base de datos');
   }
 });
   
